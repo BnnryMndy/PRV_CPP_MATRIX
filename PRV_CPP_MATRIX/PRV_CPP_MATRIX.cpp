b9c447a1220dbbb17881115225de59ee71c908af
Bnnrymndy@gmail.com <Bnnrymndy@gmail.com>
2021-10-12 07:01:20 +0300

    some style fixes

--- PRV_CPP_MATRIX/PRV_CPP_MATRIX.cpp
+++ PRV_CPP_MATRIX/PRV_CPP_MATRIX.cpp
@@ -12,7 +12,6 @@ void MultiplyMatrix(
     double(matrixTwo)[MATRIX_SIZE][MATRIX_SIZE],
     double(resultMatrix)[MATRIX_SIZE][MATRIX_SIZE]
 ) {
-    //double resultMatrix[MATRIX_SIZE][MATRIX_SIZE];
 
     for (int i = 0; i < MATRIX_SIZE; i++)
     {
@@ -41,13 +40,12 @@ void printMatrix(double(matrix)[MATRIX_SIZE][MATRIX_SIZE])
 
 void fillMatrix(double(matrix)[MATRIX_SIZE][MATRIX_SIZE])
 {
-   
 
     for (int i = 0; i < MATRIX_SIZE; i++)
     {
         for (int j = 0; j < MATRIX_SIZE; j++)
         {
-            matrix[i][j] = (double)rand()/0.00001;
+            matrix[i][j] = (double)rand() / 0.00001;
         }
     }
 }
@@ -72,21 +70,4 @@ int main()
     MultiplyMatrix(matrixOne, matrixTwo, resultMatrix);
 
     if(MATRIX_SIZE < 5) printMatrix(resultMatrix);
-
-
-    
-
-
-    std::cout << "Hello World!\n";
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
