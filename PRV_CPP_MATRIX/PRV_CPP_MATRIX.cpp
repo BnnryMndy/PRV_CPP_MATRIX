5a18892980e26dce2271936f74e2c25914b37757
Bnnrymndy@gmail.com <Bnnrymndy@gmail.com>
2021-10-13 11:23:49 +0300

    add parallel

--- PRV_CPP_MATRIX/PRV_CPP_MATRIX.cpp
+++ PRV_CPP_MATRIX/PRV_CPP_MATRIX.cpp
@@ -8,16 +8,31 @@
 
 using namespace std;
 
-size_t MATRIX_SIZE = 1000;
+int MATRIX_SIZE = 1000;
 
 void MultiplyMatrix(double** matrixOne, double** matrixTwo, double** resultMatrix)
 {
 
-    for (size_t i = 0; i < MATRIX_SIZE; i++)
+    for (int i = 0; i < MATRIX_SIZE; i++)
     {
-        for (size_t j = 0; j < MATRIX_SIZE; j++)
+        for (int j = 0; j < MATRIX_SIZE; j++)
         {
-            for (size_t k = 0; k < MATRIX_SIZE; k++)
+            for (int k = 0; k < MATRIX_SIZE; k++)
+            {
+                resultMatrix[i][j] += matrixOne[i][k] * matrixTwo[k][j];
+            }
+        }
+    }
+}
+
+void ParallelMultiplyMatrix(double** matrixOne, double** matrixTwo, double** resultMatrix)
+{
+    #pragma omp parallel for num_threads(8)
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    {
+        for (int j = 0; j < MATRIX_SIZE; j++)
+        {
+            for (int k = 0; k < MATRIX_SIZE; k++)
             {
                 resultMatrix[i][j] += matrixOne[i][k] * matrixTwo[k][j];
             }
@@ -27,9 +42,9 @@ void MultiplyMatrix(double** matrixOne, double** matrixTwo, double** resultMatri
 
 void printMatrix(double** matrix)
 {
-    for (size_t i = 0; i < MATRIX_SIZE; i++)
+    for (int i = 0; i < MATRIX_SIZE; i++)
     {
-        for (size_t j = 0; j < MATRIX_SIZE; j++)
+        for (int j = 0; j < MATRIX_SIZE; j++)
         {
             cout << matrix[i][j] << " \t";
         }
@@ -41,9 +56,9 @@ void printMatrix(double** matrix)
 void fillMatrix(double** matrix)
 {
 
-    for (size_t i = 0; i < MATRIX_SIZE; i++)
+    for (int i = 0; i < MATRIX_SIZE; i++)
     {
-        for (size_t j = 0; j < MATRIX_SIZE; j++)
+        for (int j = 0; j < MATRIX_SIZE; j++)
         {
             matrix[i][j] = (double)rand() / 0.00001;
         }
@@ -53,7 +68,7 @@ void fillMatrix(double** matrix)
 double shpoor(double** matrix)
 {
     double result = 0;
-    for (size_t i = 0; i < MATRIX_SIZE; i++)
+    for (int i = 0; i < MATRIX_SIZE; i++)
     {
         result += matrix[i][i];
     }
@@ -65,25 +80,25 @@ int main()
 {
     cin >> MATRIX_SIZE;
     double** matrixOne = new double*[MATRIX_SIZE];
-    for (size_t i = 0; i < MATRIX_SIZE; i++)
+    for (int i = 0; i < MATRIX_SIZE; i++)
     {   
         matrixOne[i] = new double[MATRIX_SIZE];
     }
     double** matrixTwo = new double* [MATRIX_SIZE];
-    for (size_t i = 0; i < MATRIX_SIZE; i++)
+    for (int i = 0; i < MATRIX_SIZE; i++)
     {
         matrixTwo[i] = new double[MATRIX_SIZE];
     }
     double** resultMatrix = new double* [MATRIX_SIZE];
-    for (size_t i = 0; i < MATRIX_SIZE; i++)
+    for (int i = 0; i < MATRIX_SIZE; i++)
     {
         resultMatrix[i] = new double[MATRIX_SIZE];
     }
 
 
-    for (size_t i = 0; i < MATRIX_SIZE; i++)
+    for (int i = 0; i < MATRIX_SIZE; i++)
     {
-        for (size_t j = 0; j < MATRIX_SIZE; j++)
+        for (int j = 0; j < MATRIX_SIZE; j++)
         {
             resultMatrix[i][j] = 0;
         }
@@ -91,9 +106,24 @@ int main()
 
     fillMatrix(matrixOne);
     fillMatrix(matrixTwo);
+
+    auto t2 = clock();
+    ParallelMultiplyMatrix(matrixOne, matrixTwo, resultMatrix);
+    cout << (float)(clock() - t2) << "\n";
+
     auto t1 = clock();
     MultiplyMatrix(matrixOne, matrixTwo, resultMatrix);
     cout << (float)(clock() - t1) << "\n";
     if (MATRIX_SIZE < 5) printMatrix(resultMatrix);
     else cout << shpoor(resultMatrix);
+    
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    {
+        for (int j = 0; j < MATRIX_SIZE; j++)
+        {
+            resultMatrix[i][j] = 0;
+        }
+    }
+
+
 }
