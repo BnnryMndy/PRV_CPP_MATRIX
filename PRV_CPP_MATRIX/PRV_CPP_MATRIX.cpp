0b728a0f7a3425951f56ff2d3b245ba2d3ec4099
Bnnrymndy@gmail.com <Bnnrymndy@gmail.com>
2021-10-13 11:09:09 +0300

    added timer

--- PRV_CPP_MATRIX/PRV_CPP_MATRIX.cpp
+++ PRV_CPP_MATRIX/PRV_CPP_MATRIX.cpp
@@ -3,21 +3,21 @@
 
 #include <iostream>
 #include <cstdlib>
-#define  MATRIX_SIZE 4
+#include <ctime>
+
 
 using namespace std;
 
-void MultiplyMatrix(
-    double(matrixOne)[MATRIX_SIZE][MATRIX_SIZE],
-    double(matrixTwo)[MATRIX_SIZE][MATRIX_SIZE],
-    double(resultMatrix)[MATRIX_SIZE][MATRIX_SIZE]
-) {
+size_t MATRIX_SIZE = 1000;
 
-    for (int i = 0; i < MATRIX_SIZE; i++)
+void MultiplyMatrix(double** matrixOne, double** matrixTwo, double** resultMatrix)
+{
+
+    for (size_t i = 0; i < MATRIX_SIZE; i++)
     {
-        for (int j = 0; j < MATRIX_SIZE; j++)
+        for (size_t j = 0; j < MATRIX_SIZE; j++)
         {
-            for (int k = 0; k < MATRIX_SIZE; k++)
+            for (size_t k = 0; k < MATRIX_SIZE; k++)
             {
                 resultMatrix[i][j] += matrixOne[i][k] * matrixTwo[k][j];
             }
@@ -25,11 +25,11 @@ void MultiplyMatrix(
     }
 }
 
-void printMatrix(double(matrix)[MATRIX_SIZE][MATRIX_SIZE])
+void printMatrix(double** matrix)
 {
-    for (int i = 0; i < MATRIX_SIZE; i++)
+    for (size_t i = 0; i < MATRIX_SIZE; i++)
     {
-        for (int j = 0; j < MATRIX_SIZE; j++)
+        for (size_t j = 0; j < MATRIX_SIZE; j++)
         {
             cout << matrix[i][j] << " \t";
         }
@@ -38,23 +38,48 @@ void printMatrix(double(matrix)[MATRIX_SIZE][MATRIX_SIZE])
 }
 
 
-void fillMatrix(double(matrix)[MATRIX_SIZE][MATRIX_SIZE])
+void fillMatrix(double** matrix)
 {
 
-    for (int i = 0; i < MATRIX_SIZE; i++)
+    for (size_t i = 0; i < MATRIX_SIZE; i++)
     {
-        for (int j = 0; j < MATRIX_SIZE; j++)
+        for (size_t j = 0; j < MATRIX_SIZE; j++)
         {
             matrix[i][j] = (double)rand() / 0.00001;
         }
     }
 }
 
+double shpoor(double** matrix)
+{
+    double result = 0;
+    for (size_t i = 0; i < MATRIX_SIZE; i++)
+    {
+        result += matrix[i][i];
+    }
+
+    return result;
+}
+
 int main()
 {
-    double matrixOne[MATRIX_SIZE][MATRIX_SIZE];
-    double matrixTwo[MATRIX_SIZE][MATRIX_SIZE];
-    double resultMatrix[MATRIX_SIZE][MATRIX_SIZE];
+    cin >> MATRIX_SIZE;
+    double** matrixOne = new double*[MATRIX_SIZE];
+    for (size_t i = 0; i < MATRIX_SIZE; i++)
+    {   
+        matrixOne[i] = new double[MATRIX_SIZE];
+    }
+    double** matrixTwo = new double* [MATRIX_SIZE];
+    for (size_t i = 0; i < MATRIX_SIZE; i++)
+    {
+        matrixTwo[i] = new double[MATRIX_SIZE];
+    }
+    double** resultMatrix = new double* [MATRIX_SIZE];
+    for (size_t i = 0; i < MATRIX_SIZE; i++)
+    {
+        resultMatrix[i] = new double[MATRIX_SIZE];
+    }
+
 
     for (size_t i = 0; i < MATRIX_SIZE; i++)
     {
@@ -66,8 +91,9 @@ int main()
 
     fillMatrix(matrixOne);
     fillMatrix(matrixTwo);
-
+    auto t1 = clock();
     MultiplyMatrix(matrixOne, matrixTwo, resultMatrix);
-
-    if(MATRIX_SIZE < 5) printMatrix(resultMatrix);
+    cout << (float)(clock() - t1) << "\n";
+    if (MATRIX_SIZE < 5) printMatrix(resultMatrix);
+    else cout << shpoor(resultMatrix);
 }
